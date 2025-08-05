21195596d66f153a9edbf3d736249900208fd536
saifulreact <saifulreact@gmail.com>
2025-08-05 15:06:12 -0700

    Initial commit

---
+++ js/scrept.js
@@ -0,0 +1,77 @@
+// let headinggame=document.querySelector(".headinggame")
+ 
+// let inputnum=document.querySelector(".inputnum")
+
+// headinggame.addEventListener("click",()=>{
+//    if(inputnum.value==""){
+//     console.log("enter a number between 1 to 9")
+//    }
+// console.log("data pacche")
+// })
+
+
+// inputnum.addEventListener("click",()=>{
+//     console.log(inputnum.value)
+// })
+// setTimeout(() => {
+//     console.log("settimeout")
+// }, 1000);
+
+// var a=20;
+// console.log(window)
+
+// let b=39;
+// console.log(window.b)
+
+// {
+//     let a=2
+// }
+// console.log(a)
+
+// {
+//     const a=33;
+
+// }
+// console.log(a)
+// function  fn(){
+//     var val=66;
+//     console.log(val);
+// }
+// function fn (){
+//     let  val=10;
+//     console.log(val)
+// }
+
+// function jus(a,b){
+//    return a+b
+// }
+// console.log(jus(10,20));
+
+
+// function jos(a,b,c){
+//     return a+b+c
+// }
+// console.log(jos(20,40,40));
+
+// function plus(a,b,c){
+//     return a+b+c
+
+// }
+// console.log(plus(10,39))
+
+
+{
+    let a=10;
+    console.log("a")
+}
+
+function jus(a =0,b=0,c=0){
+    return a+b+c
+}
+console.log(jus(10,20,30))
+
+
+function apple(a,...b){
+   console.log(b)
+}
+apple(10,390,39,98,)
